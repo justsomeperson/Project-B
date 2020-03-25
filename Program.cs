528a5b863ed6ba6e9f10c82f531ba53e636843fd
justsomeperson <59874159+justsomeperson@users.noreply.github.com>
2020-03-25 13:22:51 +0100

    idk

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PRojectBstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Voer je reserveringsnummer in");
+            string ingevoerde_nummer = Console.ReadLine();
+
+            Console.WriteLine("Voer je naam in");
+            string ingevoerde_naam = Console.ReadLine();
+
+
+            bool is_nummer_correct = ingevoerde_nummer == "0001";
+            bool is_naam_correct = ingevoerde_naam == "Ahmed";
+
+            if (is_nummer_correct && is_naam_correct){
+                Console.WriteLine("Welkom Ahmed");
+            } else
+            {
+                Console.WriteLine("Reserveringsnummer of naam is incorrect");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
